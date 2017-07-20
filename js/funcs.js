b2dff9222e7ad7159934974e307e887a05c94c1b
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2017-07-21 01:13:16 +0430

    Add functions

--- js/funcs.js
+++ js/funcs.js
@@ -2,3 +2,13 @@
 // 2017-07-19
 // ******************** 
 
+function calc()
+{
+    var x = document.getElementById('first_operand').value;
+    var y = document.getElementById('second_operand').value;
+    
+    document.getElementById('sum').innerHTML = parseInt(x) + parseInt(y);
+    document.getElementById('sub').innerHTML = parseInt(x) - parseInt(y);
+    document.getElementById('multi').innerHTML = parseInt(x) * parseInt(y);
+    document.getElementById('div').innerHTML = parseInt(x) / parseInt(y);
+}
